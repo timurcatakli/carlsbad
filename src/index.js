49243e9e6c03c1a45f7c134396cd5abb8e9fa11a
Timur Catakli <timurcatakli@gmail.com>
2018-09-21 09:46:06 -0700

    Initial install of ant design

--- src/index.js
+++ src/index.js
@@ -1,9 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Button } from 'antd';
 import './style.css';
 
 const title = 'My Minimal React Webpack Babel Setup';
 
-ReactDOM.render(<div>{title} - </div>, document.getElementById('app'));
+ReactDOM.render(
+  <div>
+    {title} - <Button type="primary">Primary</Button>
+  </div>,
+  document.getElementById('app'),
+);
 
 module.hot.accept();
